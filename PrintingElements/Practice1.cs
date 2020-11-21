800951ee06e7f9833853a407ee3da44fc1bb5149
LAPTOP-OSS12VPA\User <e4vishal@gmail.com>
2020-11-21 10:06:01 +0000

    Practice session

---
+++ PrintingElements/Practice1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingElements
+{
+    public class Practice1
+    {
+        public static void Main(string[] args)
+        {
+            string[] car = new string[3] { "BMW", "Audi", "Ford" };
+            Console.WriteLine(car[0]);
+            Console.WriteLine(car[1]);
+            Console.WriteLine(car[2]);
+        }
+    }
+}
